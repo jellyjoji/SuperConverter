15f9d36d9257df523cfed17c221e008d9d249cfe
jellyjoji <juicy_joji@yu.ac.kr>
2023-08-26 02:25:50 +0900

    build

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const [amount, setAmount] = useState(0);
-  const [inverted, setInverted] = useState(false);
-
-  const onChange = (event) => {
-    // console.log(event.target.value);
-    setAmount(event.target.value);
-  };
-  const reset = () => {
-    setAmount(0);
-  };
-  const onInvert = () => {
-    // setInverted(!inverted);
-    reset();
-    setInverted((current) => !current);
-  };
-
-  return (
-    <>
-      <h1>Time Converter</h1>
-      <label htmlFor="minutes">Minutes : </label>
-      <input
-        value={inverted ? amount * 60 : amount}
-        onChange={onChange}
-        id="minutes"
-        placeholder="Minutes"
-        type="number"
-        // this input will be disabled if inverted is true
-        disabled={inverted === true}
-      />
-
-      <br />
-      <br />
-
-      <label htmlFor="hours">Hours : </label>
-      <input
-        // value={minutes / 60}
-        value={inverted ? amount : Math.round(amount / 60)}
-        onChange={onChange}
-        id="hours"
-        placeholder="Hours"
-        type="number"
-        // readOnly
-        // it will be disabled if inverted is false
-        disabled={inverted === false}
-      />
-
-      <p>You want to convert : {amount}</p>
-
-      <button onClick={reset}>Reset</button>
-      <button onClick={onInvert}>{inverted ? "invert" : "Turn Back"}</button>
-    </>
-  );
+  return;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import MinutesToHours from "./components/MinutesToHours";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <MinutesToHours />;
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
