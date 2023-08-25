b25826587d8b739e6bf9c5631233c227a069301d
jellyjoji <juicy_joji@yu.ac.kr>
2023-08-26 02:35:58 +0900

    pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { env } from 'node:process';
+
+const isDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig()({
+  // github pages 배포 주소 변경 
+  base: "/TimeConverter/", 
+  // base: env.PUBLIC_URL, 
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+    modules: {
+      generateScopedName: isDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
