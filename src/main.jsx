fff5e6b226e1a23a6a39bbdb7c725015cca1f763
jellyjoji <juicy_joji@yu.ac.kr>
2023-08-26 03:06:18 +0900

    time converter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import MinutesToHours from "./components/MinutesToHours";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MinutesToHours />;
+    <MinutesToHours />
     <App />
   </React.StrictMode>
 );
